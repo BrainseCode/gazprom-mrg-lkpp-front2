b0a10df80db44b2ff9a54ae554e48d06df68c8f5
tpiv <ds@tpiv.ru>
2023-06-02 16:32:45 +0300

    норм

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST={}
+self.__REACT_LOADABLE_MANIFEST={"..\\components\\calorieArchive\\SearchCalorieArchive.js -> react-apexcharts":{"id":"..\\components\\calorieArchive\\SearchCalorieArchive.js -> react-apexcharts","files":["static/chunks/node_modules_react-apexcharts_dist_react-apexcharts_min_js.js"]}}
