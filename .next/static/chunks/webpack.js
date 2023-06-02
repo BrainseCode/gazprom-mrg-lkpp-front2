5e2fb1421f8f224e2aa37d99a47456f699883f3a
tpiv <ds@tpiv.ru>
2023-06-02 15:33:40 +0300

    Добавил на главной выравнивание кнопок, ровно по правому краю

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "15f86bb6c24c50f8"; }
+/******/ 		__webpack_require__.h = function() { return "3ccac07e562dc512"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
