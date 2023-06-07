0a8d845ae656a9b9d831dc9bf48a275ed2717757
Dmitry Zhunev <brainsecode@gmail.com>
2023-06-07 10:05:05 +0300

    finish prototype

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "647f743533bfbc6f"; }
+/******/ 		__webpack_require__.h = function() { return "57e8c471ae9f61f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
